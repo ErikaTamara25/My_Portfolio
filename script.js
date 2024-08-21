0b72658d37e9e2d1a71f13c297a055e8d434ed49
Erika <erikatamara2583@gmail.com>
2024-08-21 03:05:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Gracias por tu interés, puedes contactarme a través de mi correo: erikatamara2583@gmail.com');
+});
